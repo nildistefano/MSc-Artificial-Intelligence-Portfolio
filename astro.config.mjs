54898aab7abef31ddd523a8c6269f227f29053ed
Nil Distefano <72510762+nildistefano@users.noreply.github.com>
2024-11-03 09:36:57 +0100

    new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import { remarkReadingTime } from "./remark-reading-time.mjs";
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), icon()],
-  output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  output: "static",  // Switch to 'static' mode for GitHub Pages compatibility
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
